57fc05ac02fcfc68cb0d40376842029499584f5c
RedKZX <121696146+RedKZX@users.noreply.github.com>
2023-12-31 13:47:11 +0000

    Add project files.

---
+++ Boombox battery/Patches/BoomBoxPlugin.cs
@@ -0,0 +1,24 @@
+﻿using BepInEx.Logging;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boombox_battery.Patches
+{
+    [HarmonyPatch(typeof(BoomboxItem))]
+    public class BoomboxPlugin
+    {
+        private const string modGUID = "Henrehs.BoomBoxBattery";
+
+        [HarmonyPatch("Start")]
+        [HarmonyPostfix]
+        private static void PatchBattery(ref Item ___itemProperties)
+        {
+            ___itemProperties.requiresBattery = false;
+        }
+    }
+}
---
+++ Boombox battery/Plugin.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Runtime.Versioning;
+using BepInEx;
+using BepInEx.Logging;
+using Boombox_battery.Patches;
+using HarmonyLib;
+using UnityEngine;
+
+namespace Boombox_battery
+{
+    [BepInPlugin("Henrehs.BoomBoxBattery", "BoomBoxBattery", "1.0.0.0")]
+    public class BoomboxBatteryBase : BaseUnityPlugin
+    {
+        private const string modGUID = "Henrehs.BoomBoxBattery";
+        private const string modName = "BoomBoxBattery";
+        private const string modVersion = "1.0.0.0";
+
+        private readonly Harmony harmony = new Harmony("Henrehs.BoomBoxBattery");
+        private static BoomboxBatteryBase Instance;
+        internal ManualLogSource mls;
+
+        void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+            }
+
+            mls = BepInEx.Logging.Logger.CreateLogSource(modGUID);
+            mls.LogInfo("[HENREH] The Boombox will now play for all eternity!");
+
+
+
+            harmony.PatchAll(typeof(BoomboxPlugin));
+        }
+    }
+}
+    
+
+   
